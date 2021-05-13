487a4958465bf2f8b3446fc6d22f941e44915b50
Simon lackmann <>
2021-05-13 20:29:24 +0200

    modified select  functionality

--- app.js
+++ app.js
@@ -7,6 +7,25 @@ const pageload = document.querySelector("#progress");
 const title = document.querySelector("#aktuell");
 const selectBL = document.querySelector("#selectBL");
 
+const blHsh = {
+  "Schleswig-Holstein": 1,
+  Hamburg: 2,
+  Niedersachsen: 3,
+  Bremen: 4,
+  "Nordrhein-Westfalen": 5,
+  Hessen: 6,
+  "Rheinland-Pfalz": 7,
+  "Baden-Württemberg": 8,
+  Bayern: 9,
+  Saarland: 10,
+  Brandenburg: 12,
+  "Mecklenburg-Vorpommern": 13,
+  Sachsen: 14,
+  "Sachsen-Anhalt": 15,
+  Thüringen: 16,
+  Berlin: 11,
+};
+
 let cov = new Covid(process.env.URL);
 
 var ctx = document.getElementById("myChart").getContext("2d");
@@ -14,37 +33,47 @@ let _labels = [];
 let _data = [];
 let _color = [];
 let _last_update = "";
+let chartMap = new Map();
 let chartSet = new Set();
+let chartSetBlNames = new Set();
+let chartSetBlId = new Set();
+let arrBlIds = [];
 
 init();
 
 selectBL.addEventListener("change", (e) => {
-  console.log(e);
+  let val = e.target.value;
+  console.log(val);
+  // if (val !== "null") covid19Chart(val);
+  // return;
 });
 
 function init() {
   cov.getData().then((i) => {
     i.features.forEach((item) => {
-      console.log(item);
-      const bl = item.attributes.BL;
-      chartSet.add(bl);
+      // console.log(item);
+      let bl = item.attributes.BL;
+      let bl_id = item.attributes.BL_ID;
+      chartSet.add(item);
+      chartSetBlNames.add(bl);
     });
-    for (let bl of chartSet) {
+    chartSetBlNames.forEach((bl) => {
       let op = document.createElement("option");
-      op.value = bl;
+      // console.log(bl);
       op.innerText = bl;
+      op.value = blHsh[bl];
       selectBL.appendChild(op);
-    }
+    });
   });
 }
 
-function covid19Chart() {
+function covid19Chart(id) {
   pageload.classList.add("is-active");
   cov.getData().then((i) => {
     _last_update = " Aktualisierung: " + i.features[0].attributes.last_update;
     i.features.forEach((item) => {
       console.log(i);
-      if (item.attributes.BL_ID !== "5") return;
+      if (item.attributes.BL_ID !== id) return;
       if (item.attributes.cases7_per_100k > 100) {
         _color.push("rgb(255, 99, 132)");
       } else {
