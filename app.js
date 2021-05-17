be8823bf0fc061a793134410e061a7af427c4134
SimHub <>
2021-05-17 12:27:30 +0200

    modified background-color

--- app.js
+++ app.js
@@ -107,9 +107,16 @@ function covid19Chart(id) {
           },
         ],
       },
-      options: {
-        plugins: {
-          decimation: _decimation,
+      plugins: {
+        decimation: _decimation,
+        id: "custom_canvas_background_color",
+        beforeDraw: (chart) => {
+          const ctx = chart.canvas.getContext("2d");
+          ctx.save();
+          ctx.globalCompositeOperation = "destination-over";
+          ctx.fillStyle = "black";
+          ctx.fillRect(0, 0, chart.width, chart.height);
+          ctx.restore();
         },
       },
     });
